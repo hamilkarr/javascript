50c90aeed428a0ecfabaf6af1a38e4c5680d67b6
hamilkarr <12uclock@gmail.com>
2021-11-16 14:50:04 +0900

    복습시작

---
+++ 복습/exam/script.js
@@ -0,0 +1,7 @@
+// window.addEventListener("DOMContentLoaded", function () {
+//   const button = document.getElementById("button");
+//   console.log("button: ", button);
+// });
+
+const button = document.getElementById("button");
+console.log("button: ", button);
