7b1acb22a031f3d8b5964a47d1a6902f3a134398
hamilkarr <12uclock@gmail.com>
2021-11-24 18:12:35 +0900

    11

--- 복습/exam/script.js
+++ 복습/exam/script.js
@@ -1,7 +1,23 @@
-// window.addEventListener("DOMContentLoaded", function () {
-//   const button = document.getElementById("button");
-//   console.log("button: ", button);
-// });
+class Person {
+  constructor(age, name) {
+    this.name = name;
+    this.age = age;
+    Person.count = Person.count || 0;
+    ++Person.count; // 정적변수
+  }
+  print() {
+    console.log(this.age, this.name);
+  }
+  static getCount() {
+    return Person.count;
+  }
+}
 
-const button = document.getElementById("button");
-console.log("button: ", button);
+class A {
+  constructor() {
+    this.a = 1;
+    this.b = 2;
+  }
+}
+
+class B extends A {}
