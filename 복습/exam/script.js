6f64334bc8a22f0ca0118ff76af5a05c5f4babf6
hamilkarr <12uclock@gmail.com>
2021-11-26 17:17:28 +0900

    222

--- 복습/exam/script.js
+++ 복습/exam/script.js
@@ -1,23 +1,4 @@
-class Person {
-  constructor(age, name) {
-    this.name = name;
-    this.age = age;
-    Person.count = Person.count || 0;
-    ++Person.count; // 정적변수
-  }
-  print() {
-    console.log(this.age, this.name);
-  }
-  static getCount() {
-    return Person.count;
-  }
-}
+const a = ['apple', 'mango', 'banana'];
 
-class A {
-  constructor() {
-    this.a = 1;
-    this.b = 2;
-  }
-}
-
-class B extends A {}
+const b = a.slice(0);
+console.log(b);
